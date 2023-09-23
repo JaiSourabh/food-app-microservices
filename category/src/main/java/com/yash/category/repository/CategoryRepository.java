922af9902a6cb6dc2b147745cfa900e882ebd40e
Sourabh Jain <sourabh.jain@Yash.com>
2023-09-23 13:20:30 +0530

    delete all content

--- category/src/main/java/com/yash/category/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yash.category.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.yash.category.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-}
--- item/src/main/java/com/yash/item/model/Category.java
+++
@@ -1,7 +0,0 @@
-package com.yash.item.model;
-
-public class Category {
-	private Integer categoryId;
-	private String categoryName;
-	private String description;
-}
